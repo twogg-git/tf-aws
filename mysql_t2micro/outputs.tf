b57999fc6d0489375e9b619bfdbee8b81f4eedc0
Catherin Cruz <cruz.catherin@gmail.com>
2019-10-06 23:55:19 -0500

    Adding mysql initial files

--- mysql_t2micro/outputs.tf
+++ mysql_t2micro/outputs.tf
@@ -1,8 +1,8 @@
 output "address" {
-  value       = aws_db_instance.mysql_t2micro.address
+  value       = "${aws_db_instance.mysql_t2micro.address}"
   description = "Connect to the database at this endpoint"
 }
 output "port" {
-  value       = aws_db_instance.mysql_t2micro.port
+  value       = "${aws_db_instance.mysql_t2micro.port}"
   description = "The port the database is listening on"
 }
