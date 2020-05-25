2d575dd2d912cc5fa9f3b3dd12553a45cce1f7a3
twogg-git <cruz.catherin@gmail.com>
2020-05-25 12:42:41 -0500

    Updating format and idents

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -5,55 +5,55 @@ variable "region" {
 }
 
 variable "tags" {
-	description = "List of tags to attach to resources"
-	type = map
+  description = "List of tags to attach to resources"
+  type = map
 }
 
 variable "inbound_ports" {
-	description = "List of inbound ports to open"
-	type = list
+  description = "List of inbound ports to open"
+  type = list
 }
 
 # Optional variables
 variable "instance_type" {
-	description = "EC2 type"
-	type = string
-	default = "t2.micro"
+  description = "EC2 type"
+  type = string
+  default = "t2.micro"
 }
 
 variable "key_pair" {
-	description = "EC2 key pair name"
-	type = string
-	default = ""
+  description = "EC2 key pair name"
+  type = string
+  default = ""
 }
 
 variable "ebs" {
-	description = "True to create and attach EBS volume"
-	type = map
-	default = {
-		status = false
-		size = 0
-		type = ""
-	}	
+  description = "True to create and attach EBS volume"
+  type = map
+  default = {
+    status = false
+    size = 0
+    type = ""
+  }	
 }
 
 # Static variables
 variable "cidr_block_all" {
-	description = "All cidr block"
-	type = string
-	default = "0.0.0.0/0"
+  description = "All cidr block"
+  type = string
+  default = "0.0.0.0/0"
 }
 
 # Dinamyc AMI loading
 data "aws_ami" "amazon-linux-2" {
-	most_recent = true
-	owners 			= ["amazon"]
-	filter {
-		name   	= "owner-alias"
-		values 	= ["amazon"]
-	}
-	filter {
-		name   	= "name"
-		values 	= ["amzn2-ami-hvm*"]
-	}
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name     = "owner-alias"
+    values   = ["amazon"]
+  }
+  filter {
+    name     = "name"
+    values   = ["amzn2-ami-hvm*"]
+  }
 }
